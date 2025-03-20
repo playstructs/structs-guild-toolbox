a0d4bb004fb86e4a720a66d4afc1041e831994c8
abstrct <abstrct@slow.ninja>
2025-03-20 11:28:02 -0400

    Added initial guild tools

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # structs-guild-toolbox
-Scripts and such for Guild administration
+**structs-guild-toolbox** provides some helpful scripts for administrating your guild. 
+
+Most players within the Structs ecosystem will not need to operate the code in this repository.
+
+## Structs
+In the distant future the species of the galaxy are embroiled in a race for Alpha Matter, the rare and dangerous substance that fuels galactic civilization. Players take command of Structs, a race of sentient machines, and must forge alliances, conquer enemies and expand their influence to control Alpha Matter and the fate of the galaxy.
+
+[Structs](https://playstructs.com) is a decentralized game in the Cosmos ecosystem, operated and governed by our community of players--ensuring Structs remains online as long as there are players to play it.
+
+
+## Learn more
+
+- [Structs](https://playstructs.com)
+- [Project Wiki](https://watt.wiki)
+- [@PlayStructs Twitter](https://twitter.com/playstructs)
+
+
+## License
+
+Copyright 2021 [Slow Ninja Inc](https://slow.ninja).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ create_guild.sh
@@ -0,0 +1,108 @@
+SLEEP=6
+
+# Probably don't change
+# Unless you used a non-standard wallet or home directory
+PARAMS_TX=" --gas auto --yes=true "
+echo "${PARAMS_TX}"
+PARAMS_QUERY="--output json"
+echo "${PARAMS_QUERY}"
+PARAMS_KEYS=" --output json"
+echo "${PARAMS_KEYS}"
+
+
+# Ask for Validator Account Name
+echo "What is the name of the structsd account for your validator?:"
+read -r VALIDATOR_ACCOUNT_NAME
+
+
+# Load the Player ID
+PLAYER_ADDRESS=`structsd ${PARAMS_KEYS} keys show ${VALIDATOR_ACCOUNT_NAME} | jq -r .address`
+echo "${PLAYER_ADDRESS}"
+
+PLAYER_ID=`structsd ${PARAMS_QUERY} query structs address ${PLAYER_ADDRESS} | jq -r .playerId`
+echo "${PLAYER_ID}"
+
+
+# Create Allocation from Player
+PLAYER_CAPACITY=`structsd ${PARAMS_QUERY} query structs player ${PLAYER_ID} | jq -r .gridAttributes.capacity`
+echo "${PLAYER_CAPACITY}"
+structsd ${PARAMS_TX} tx structs allocation-create ${PLAYER_ID} ${PLAYER_CAPACITY} --allocation-type automated --from ${VALIDATOR_ACCOUNT_NAME}
+sleep $SLEEP
+ALLOCATION_ID=`structsd query structs allocation-all-by-source ${PLAYER_1_ID} --output json | jq -r .Allocation[0].id`
+
+# Create Substation 1
+structsd ${PARAMS_TX} tx structs substation-create ${PLAYER_1_ID} ${ALLOCATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
+sleep $SLEEP
+SUBSTATION_ID=`structsd ${PARAMS_QUERY} query structs allocation ${PLAYER_1_ID}-0 | jq -r ".Allocation.destinationId"`
+
+# Create Guild
+structsd ${PARAMS_TX} tx structs guild-create "" ${SUBSTATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
+sleep $SLEEP
+
+
+# Get the new Guild ID
+GUILD_ID=`structsd ${PARAMS_QUERY} query structs player ${PLAYER_ID} | jq -r .Player.guildId`
+echo "${GUILD_ID}"
+
+echo "Please provide as much or as little details about your Guild..."
+
+# Create the Guild Definition
+echo "Guild Name:"
+read -r GUILD_NAME
+
+echo "Guild Description:"
+read -r GUILD_DESCRIPTION
+
+echo "Guild Tag (a 1-3 character gamer tag):"
+read -r GUILD_TAG
+
+echo "A URL to a Guild Logo:"
+read -r GUILD_LOGO
+
+echo "A URL to a Guild Website:"
+read -r GUILD_WEBSITE
+
+echo "What is the ticker for your Guild Currency? (Ex gld):"
+read -r GUILD_TOKEN_NAME
+
+echo "What is the smallest unit of your Guild Currency called? (Ex dst):"
+read -r GUILD_TOKEN_SMALLEST_VALUE_NAME
+
+
+echo "Provide a public discord username contact (or don't, nbd):"
+read -r GUILD_SOCIAL_DISCORD_CONTACT
+
+GUILD_JSON=$( jq -n \
+                  --arg id "$GUILD_ID" \
+                  --arg name "$GUILD_NAME" \
+                  --arg description "$GUILD_DESCRIPTION" \
+                  --arg tag "$GUILD_TAG" \
+                  --arg logo "$GUILD_LOGO" \
+                  --arg website "$GUILD_WEBSITE" \
+                  --arg bluesky "$GUILD_SOCIAL_BLUESKY" \
+                  --arg facebook "$GUILD_SOCIAL_FACEBOOK" \
+                  --arg farcaster "$GUILD_SOCIAL_FARCASTER" \
+                  --arg instagram "$GUILD_SOCIAL_INSTAGRAM" \
+                  --arg twitch "$GUILD_SOCIAL_TWITCH" \
+                  --arg x "$GUILD_SOCIAL_X" \
+                  --arg youtube "$GUILD_SOCIAL_YOUTUBE" \
+                  --arg discordContact "$GUILD_SOCIAL_DISCORD_CONTACT" \
+                  --arg discordServer "$GUILD_SOCIAL_DISCORD_SERVER" \
+                  --arg telegramContact "$GUILD_SOCIAL_TELEGRAM_CONTACT" \
+                  --arg telegramChannel "$GUILD_SOCIAL_TELEGRAM_CHANNEL" \
+                  --arg coin "$GUILD_TOKEN_NAME" \
+                  --arg smallestCoin "$GUILD_TOKEN_SMALLEST_VALUE_NAME" \
+                  --arg guildApi "$GUILD_SERVICE_GUILD_API" \
+                  --arg reactorApi "$GUILD_SERVICE_REACTOR_API" \
+                  --arg explorer "$GUILD_SERVIVCE_EXPLORER" \
+								'{ guild: { id: $id, name: $name, description: $description, tag: $tag, logo: $logo, website: $website, socials: { bluesky: $bluesky, facebook: $facebook, farcaster: $farcaster, instagram: $instagram, twitch: $twitch, x: $x, youtube: $youtube, discord_contact: $discordContact, discord_server: $discordServer, telegram_contact: $telegramContact, telegram_channel: $telegramChannel }, denom: {"6": $coin, "0": $smallestCoin }, services:{ guild_api: $guildApi, reactorApi: $reactorApi, explorer: $explorer  } } }' )
+
+echo $GUILD_JSON > guild.json.tmp
+UPLOAD_URL=$(curl --upload-file 'guild.json.tmp' 'https://paste.c-net.org/')
+sleep $SLEEP
+
+echo "Updating Endpoint..."
+structsd ${PARAMS_TX} tx structs guild-update-endpoint ${GUILD_ID} "${UPLOAD_URL}"  --from ${VALIDATOR_ACCOUNT_NAME}
+sleep $SLEEP
+
+echo "Guild Creation Complete"
