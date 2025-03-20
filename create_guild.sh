8ec7fde4fce79c2e16b8469f189c8013945ef6f6
abstrct <abstrct@slow.ninja>
2025-03-20 17:41:47 -0400

    Fixed issues in create_guild.sh

--- create_guild.sh
+++ create_guild.sh
@@ -23,27 +23,67 @@ PLAYER_ID=`structsd ${PARAMS_QUERY} query structs address ${PLAYER_ADDRESS} | jq
 echo "${PLAYER_ID}"
 
 
+
+
+
 # Create Allocation from Player
+echo "Checking current Player available capacity"
 PLAYER_CAPACITY=`structsd ${PARAMS_QUERY} query structs player ${PLAYER_ID} | jq -r .gridAttributes.capacity`
 echo "${PLAYER_CAPACITY}"
-structsd ${PARAMS_TX} tx structs allocation-create ${PLAYER_ID} ${PLAYER_CAPACITY} --allocation-type automated --from ${VALIDATOR_ACCOUNT_NAME}
-sleep $SLEEP
 
+
+
+echo "Checking to see if an allocation exists..."
 ALLOCATION_ID=`structsd query structs allocation-all-by-source ${PLAYER_ID} --output json | jq -r .Allocation[0].id`
 
-# Create Substation 1
-structsd ${PARAMS_TX} tx structs substation-create ${PLAYER_ID} ${ALLOCATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
-sleep $SLEEP
-SUBSTATION_ID=`structsd ${PARAMS_QUERY} query structs allocation ${PLAYER_ID} | jq -r ".Allocation.destinationId"`
+if [[ -z "${ALLOCATION_ID}" ]] || [ "$ALLOCATION_ID" == "null" ]; then
+  echo "Creating a new allocation"
+  structsd ${PARAMS_TX} tx structs allocation-create ${PLAYER_ID} ${PLAYER_CAPACITY} --allocation-type automated --from ${VALIDATOR_ACCOUNT_NAME}
+  sleep $SLEEP
 
-# Create Guild
-structsd ${PARAMS_TX} tx structs guild-create "" ${SUBSTATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
-sleep $SLEEP
+  ALLOCATION_ID=`structsd query structs allocation-all-by-source ${PLAYER_ID} --output json | jq -r .Allocation[0].id`
+else
+  echo "Allocation already found"
+fi
+echo "${ALLOCATION_ID}"
 
 
-# Get the new Guild ID
+
+echo "Checking for Substation..."
+SUBSTATION_ID=`structsd ${PARAMS_QUERY} query structs allocation ${ALLOCATION_ID} | jq -r ".Allocation.destinationId"`
+if [[ -z "${SUBSTATION_ID}" ]] || [ "$SUBSTATION_ID" == "null" ]; then
+  echo "Creating a new Substation"
+
+  # Create Substation 1
+  structsd ${PARAMS_TX} tx structs substation-create ${PLAYER_ID} ${ALLOCATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
+  sleep $SLEEP
+  SUBSTATION_ID=`structsd ${PARAMS_QUERY} query structs allocation ${ALLOCATION_ID} | jq -r ".Allocation.destinationId"`
+
+else
+  echo "Substation already found"
+fi
+
+echo "${SUBSTATION_ID}"
+
+
+echo "Checking to see if Guild Already Exists"
 GUILD_ID=`structsd ${PARAMS_QUERY} query structs player ${PLAYER_ID} | jq -r .Player.guildId`
-echo "${GUILD_ID}"
+if [[ -z "${GUILD_ID}" ]] || [ "$GUILD_ID" == "null" ]; then
+  echo "Creating a new Giold"
+  # Create Guild
+  structsd ${PARAMS_TX} tx structs guild-create "temp.endpoint.com" ${SUBSTATION_ID} --from ${VALIDATOR_ACCOUNT_NAME}
+  sleep $SLEEP
+  # Get the new Guild ID
+  GUILD_ID=`structsd ${PARAMS_QUERY} query structs player ${PLAYER_ID} | jq -r .Player.guildId`
+else
+  echo "Guild already found "
+fi
+
+
+if [[ -z "${GUILD_ID}" ]] || [ "$GUILD_ID" == "null" ]; then
+  echo "Problem during Creation!"
+  exit
+fi
 
 echo "Please provide as much or as little details about your Guild..."
 
